07eec1717d1e95f5b0ce3ff264317aaae04b0dcc
Schweidzon <dannielsch@gmail.com>
2022-12-18 11:03:24 -0300

    Refatorando função de mostrar a barra de progresso dos hábitos do dia

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -97,6 +97,7 @@ const RegisterContainer = styled.div`
                 border: 1px solid #D5D5D5;
                 border-radius: 5px;
                 padding: 7px;
+                font-size: 16px;
                 &::placeholder {
                     font-style: normal;
                     font-weight: 400;
